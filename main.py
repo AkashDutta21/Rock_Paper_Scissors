4ca4bb197c139c92216fcebfa7583363e4df7536
EvoFRost <70853121+AkashDutta21@users.noreply.github.com>
2023-02-25 06:42:53 +0000

    Completed the Game Development

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import random
+
 def get_choices():
-  player_choice = "rock"
-  computer_choice = "paper"
+  options = ["rock", "paper", "scissors"]
+  player_choice = input("Enter a choice [rock/paper/scissors]: ")
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}  
+  return choices
 
-  return computer_choice
+def check_win(player, computer):
+  print(f"You chose {player}, Computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+  
+  elif player == "rock": 
+    if computer == "scissors":
+      return "Rock smashes Scissors! You win!!"
+    else:
+      return "Paper covers Rock! You lose."
 
+  elif player == "paper": 
+    if computer == "rock":
+      return "Paper covers Rock! You Win!!"
+    else:
+      return "Scissors cuts through paper! You lose."
+
+  elif player == "scissors": 
+    if computer == "rock":
+      return "Rock smashes Scissors! You lose."
+    else:
+      return "Scissors cuts through paper! You Win!!"
+
+choices = get_choices()
+
+result = check_win(choices["player"], choices["computer"])
+print(result)
